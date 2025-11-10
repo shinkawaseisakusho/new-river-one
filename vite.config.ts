881f87b31ecdcce23db1cfd6e4afc8bf6c43bcdf
shotatani <139521635+shotatani@users.noreply.github.com>
2025-11-10 14:20:40 +0900

    chore: GitHub Pages setup (base & workflow)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/new-river-one/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
